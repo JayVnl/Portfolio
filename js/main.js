db2796824bbe9308929ac240446afa0a02590c91
JayVnl <jorrit.v@outlook.com>
2020-08-11 21:22:31 +0200

    added navigation to projects from work page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+// TOP BAR NAVIGATION
 $(document).on("click", ".link", function (event) {
   let destinationName = event.target.innerHTML;
   let destination = document.getElementById(destinationName);
   let origin = document.querySelector(".active");
   let menu = $("#top-menu");
+  let link = $("#top-menu span");
 
   if (origin.id === "home") {
     menu.fadeIn(800);
@@ -10,6 +12,37 @@ $(document).on("click", ".link", function (event) {
     menu.fadeOut(800);
   }
 
+  pageAnimation(origin, destination);
+
+  for (let i = 0; i < link.length; i++) {
+    link[i].innerHTML === destinationName
+      ? (link[i].style.borderBottom = "solid 1px #fff")
+      : (link[i].style.borderBottom = "solid 1px transparent");
+  }
+});
+
+// NAVBAR BACKGROUND ON SCROLL
+$(window).on("scroll", function () {
+  $(window).scrollTop() > 70
+    ? $("#top-menu .bg").css("background", "rgba(0, 0, 0, .8)")
+    : $("#top-menu .bg").css("background", "transparent");
+});
+
+// WORK TO PROJECT NAVIGATION
+$(document).on("click", ".box", function (event) {
+  let origin = document.querySelector(".active");
+  let selected = event.target.id.slice(2);
+  let destination = document.getElementById(selected);
+  let link = $("#top-menu span");
+
+  pageAnimation(origin, destination);
+
+  link[1].style.borderBottom = "solid 1px transparent";
+});
+
+// FUNCTIONS
+
+function pageAnimation(origin, destination) {
   origin.classList.remove("active");
   destination.classList.add("active");
 
@@ -24,18 +57,4 @@ $(document).on("click", ".link", function (event) {
   setTimeout(() => {
     destination.classList.remove("animation-page-show");
   }, 2000);
-
-  let link = $("#top-menu span");
-
-  for (let i = 0; i < link.length; i++) {
-    link[i].innerHTML === destinationName
-      ? (link[i].style.borderBottom = "solid 1px #fff")
-      : (link[i].style.borderBottom = "solid 1px transparent");
-  }
-});
-
-$(window).on("scroll", function () {
-  $(window).scrollTop() > 70
-    ? $("#top-menu .bg").css("background", "rgba(0, 0, 0, .8)")
-    : $("#top-menu .bg").css("background", "transparent");
-});
+}
